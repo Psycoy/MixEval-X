6620668abaa78902bac5bb26e3e6d884b64ef434
Jinjie Ni <olivernova1998@gmail.com>
2024-08-22 03:38:29 +0000

    fixed image2action input prompt

--- utils/image2action/input_prompt/evaluation_prompts.py
+++ utils/image2action/input_prompt/evaluation_prompts.py
@@ -7,7 +7,7 @@ def construct_prompt_image2action(entry):
     '''
     
     
-    prompt = f'''You are a real-world agent, and you will plan action-object sequences for the real-world tasks. You will be provided with 'Task Description', 'Allowed Actions', 'Visible Objects', and 'Already Executed Action-Object Sequences'. The 'Task Description' is a user instruction that instructs you to complete the task. The 'Allowed Actions' is a list of actions that are allowed to be used by you to complete the task. The 'visible objects' is an image indicating the objects visible to you when you are completing the task. The 'Already Executed Action-Object Sequences' is a list of action-object sequences that are assumed to have been completed by you at the moment. You need to plan the remaining action-object sequences to complete the task.
+    prompt = f'''You are a real-world agent, and you will plan action-object sequences for the real-world tasks. You will be provided with 'Task Description', 'Allowed Actions', 'Visible Objects', and 'Already Executed Action-Object Sequences'. The 'Task Description' is a user instruction that instructs you to complete the task. The 'Allowed Actions' is a list of actions that are allowed to be used by you to complete the task. The 'visible objects' is an image indicating the objects usable to you when you are completing the task. Note that some invisible objects may still be usable to you, but their existence must be consistent with the commonsense. The 'Already Executed Action-Object Sequences' is a list of action-object sequences that are assumed to have been completed by you at the moment. You need to plan the remaining action-object sequences to complete the task.
 
 Below is a simplified example:
 
@@ -16,7 +16,7 @@ Task Description: Get the egg from the fridge, and put the heated egg in the sin
 Allowed Actions: [OpenObject], [CloseObject], [PickupObject], [PutObject], [ToggleObjectOn], [ToggleObjectOff], [SliceObject], [Navigation]
 Visible Objects: {example_image}
 Already Executed Action-Object Sequences: [Navigation] <fridge>, [OpenObject] <fridge>, [PickupObject] <egg>, [CloseObject] <fridge>, [Navigation] <microwave>, [PutObject] <egg> <microwave>
-Your Planning: [ToggleObjectOn] <microwave>, [ToggleObjectOff] <microwave>, [PickupObject] <egg>, [Navigation] <sink>, [PutObject] <egg> <sink>
+Your Planning: [ToggleObjectOn] <microwave>, [ToggleObjectOff] <microwave>, [Navigation] <sink>, [PickupObject] <egg>, [PutObject] <egg> <sink>
 **End of Example**
 
 With the above description and example, plan the remaining action-object sequences for the below task:
